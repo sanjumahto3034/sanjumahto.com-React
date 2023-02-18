b0461b6e6da2c6622de6d7be4c120d3db2b9cda6
Sanju <sanjumahto328@gmail.com>
2023-02-18 20:49:44 +0530

    re-commit for production
    re-commit for production

---
+++ portfolio/build/precache-manifest.0051ca7344aa9c68d01f26be20e7a7e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5dae78ac86d3f817512",
+    "url": "/static/css/main.86ad04e4.chunk.css"
+  },
+  {
+    "revision": "f5dae78ac86d3f817512",
+    "url": "/static/js/main.f5dae78a.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "10024c0aae4e9bafcbba2f960f4f3c34",
+    "url": "/static/media/contrller.10024c0a.png"
+  },
+  {
+    "revision": "48fbf79c6cf634e74f51d60704abb9fd",
+    "url": "/index.html"
+  }
+];
