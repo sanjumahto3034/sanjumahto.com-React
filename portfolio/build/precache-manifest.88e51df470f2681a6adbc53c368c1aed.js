c93324ee5c4e384df0080ec6ff26288743ca0619
Sanju <sanjumahto328@gmail.com>
2023-02-18 15:53:35 +0530

    code merge and update

---
+++ portfolio/build/precache-manifest.88e51df470f2681a6adbc53c368c1aed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c725e388e2259ab43d87",
+    "url": "/static/css/main.dc912888.chunk.css"
+  },
+  {
+    "revision": "c725e388e2259ab43d87",
+    "url": "/static/js/main.c725e388.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "b9842c9c4c236a614e5d",
+    "url": "/static/js/runtime~main.b9842c9c.js"
+  },
+  {
+    "revision": "10024c0aae4e9bafcbba2f960f4f3c34",
+    "url": "/static/media/contrller.10024c0a.png"
+  },
+  {
+    "revision": "c5dc3c20e491f9097956ee55306e194d",
+    "url": "/index.html"
+  }
+];
