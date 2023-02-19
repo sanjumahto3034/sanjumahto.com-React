dac6fe5569de1af38a3303c3d517b7269b4b86a2
Sanju <sanjumahto328@gmail.com>
2023-02-19 14:10:53 +0530

    up update

---
+++ portfolio/build/precache-manifest.165a68e9cc2dab6e01f63bd5a250ccd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "8580af7b3f52c368602c",
+    "url": "/static/css/main.85086aaf.chunk.css"
+  },
+  {
+    "revision": "8580af7b3f52c368602c",
+    "url": "/static/js/main.8580af7b.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "b56db25ab1c8e8106f9d",
+    "url": "/static/js/2.b56db25a.chunk.js"
+  },
+  {
+    "revision": "c13a65875c5c3d089b96",
+    "url": "/static/js/runtime~main.c13a6587.js"
+  },
+  {
+    "revision": "10024c0aae4e9bafcbba2f960f4f3c34",
+    "url": "/static/media/contrller.10024c0a.png"
+  },
+  {
+    "revision": "e75a7cac8f0d8c87ab745876e6895d5d",
+    "url": "/static/media/boardgame.e75a7cac.png"
+  },
+  {
+    "revision": "f9a697366d4b3c0ace3ec37dbef3315b",
+    "url": "/index.html"
+  }
+];
