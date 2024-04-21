a7b21d293bb4ec94a0a2fb7259e854b644a579c5
Sanju Mahto <sanjumahto328@gmail.com>
2024-04-21 18:07:29 +0530

    update logo

--- portfolio/build/precache-manifest.d381ef36711682fd7edff0c06076aabd.js
+++ portfolio/build/precache-manifest.d381ef36711682fd7edff0c06076aabd.js
@@ -60,24 +60,12 @@ self.__precacheManifest = [
     "url": "/static/media/ludo_game.4b4eed74.jpg"
   },
   {
-    "revision": "90190d91283189e340b2a44fe560f2cd",
-    "url": "/static/media/RobotoMono-Bold.90190d91.ttf"
-  },
-  {
     "revision": "c9166464b1db95fc3cdf9b50fc7f98e2",
     "url": "/static/media/RobotoMono-Light.c9166464.ttf"
   },
   {
     "revision": "896a60219f6157eab096825a0c9348a8",
     "url": "/static/media/Ubuntu-Bold.896a6021.ttf"
-  },
-  {
-    "revision": "642c61c3093f360ebfe8c96dce691e21",
-    "url": "/static/media/RobotoMono-ExtraLight.642c61c3.ttf"
-  },
-  {
-    "revision": "5b04fdfec4c8c36e8ca574e40b7148bb",
-    "url": "/static/media/RobotoMono-Regular.5b04fdfe.ttf"
   },
   {
     "revision": "c9d68f61bf89fde6a24da697bc45d6f9",
@@ -92,8 +80,28 @@ self.__precacheManifest = [
     "url": "/static/media/RobotoMono-Medium.0eaa3f45.ttf"
   },
   {
+    "revision": "90190d91283189e340b2a44fe560f2cd",
+    "url": "/static/media/RobotoMono-Bold.90190d91.ttf"
+  },
+  {
+    "revision": "5b04fdfec4c8c36e8ca574e40b7148bb",
+    "url": "/static/media/RobotoMono-Regular.5b04fdfe.ttf"
+  },
+  {
+    "revision": "642c61c3093f360ebfe8c96dce691e21",
+    "url": "/static/media/RobotoMono-ExtraLight.642c61c3.ttf"
+  },
+  {
     "revision": "eabbacb68fea19c24a80eaed6715ca4a",
     "url": "/static/media/RobotoMono-SemiBold.eabbacb6.ttf"
+  },
+  {
+    "revision": "ed645c2b20d22612c4985bc4e4b4a7ff",
+    "url": "/static/media/Raleway-Light.ed645c2b.ttf"
+  },
+  {
+    "revision": "d3c3b35e6d478ed149f02fad880dd359",
+    "url": "/static/media/Ubuntu-Medium.d3c3b35e.ttf"
   },
   {
     "revision": "564755b4222c6321216f1e764c9204f9",
@@ -104,40 +112,32 @@ self.__precacheManifest = [
     "url": "/static/media/Raleway-Thin.db8e7354.ttf"
   },
   {
-    "revision": "ed645c2b20d22612c4985bc4e4b4a7ff",
-    "url": "/static/media/Raleway-Light.ed645c2b.ttf"
-  },
-  {
-    "revision": "550ad10fece0b35b0325d38192701592",
-    "url": "/static/media/Raleway-ExtraLight.550ad10f.ttf"
-  },
-  {
-    "revision": "d3c3b35e6d478ed149f02fad880dd359",
-    "url": "/static/media/Ubuntu-Medium.d3c3b35e.ttf"
-  },
-  {
-    "revision": "87641f9900d717d6bfbf108b8755868e",
-    "url": "/static/media/Raleway-SemiBold.87641f99.ttf"
+    "revision": "20d41940068e45b114d32b48d684ef39",
+    "url": "/static/media/Raleway-Regular.20d41940.ttf"
   },
   {
     "revision": "b952c3c81ba34b54c66c748ea1e828a7",
     "url": "/static/media/Raleway-Medium.b952c3c8.ttf"
   },
   {
-    "revision": "20d41940068e45b114d32b48d684ef39",
-    "url": "/static/media/Raleway-Regular.20d41940.ttf"
+    "revision": "88079335418f389bfb2d86bc4f1ced64",
+    "url": "/static/media/Raleway-Bold.88079335.ttf"
+  },
+  {
+    "revision": "87641f9900d717d6bfbf108b8755868e",
+    "url": "/static/media/Raleway-SemiBold.87641f99.ttf"
   },
   {
     "revision": "27f7ef17de3691b5cdb9f1ee1ee5cc6a",
     "url": "/static/media/Raleway-ExtraBold.27f7ef17.ttf"
   },
   {
-    "revision": "88079335418f389bfb2d86bc4f1ced64",
-    "url": "/static/media/Raleway-Bold.88079335.ttf"
-  },
-  {
     "revision": "35e0e2e7a5b03275ba569a214edbab77",
     "url": "/static/media/Raleway-Black.35e0e2e7.ttf"
+  },
+  {
+    "revision": "550ad10fece0b35b0325d38192701592",
+    "url": "/static/media/Raleway-ExtraLight.550ad10f.ttf"
   },
   {
     "revision": "760a2e84aa5777084dd3f02f685c95f9",
--- portfolio/build/service-worker.js
+++ portfolio/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63cba8b5fcb023e2aa543a1b31d7c587.js"
+  "/precache-manifest.d381ef36711682fd7edff0c06076aabd.js"
 );
 
 workbox.clientsClaim();
